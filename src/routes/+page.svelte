6e6f16e8d38f296ba26fb080c29dc82ecebe7a2e
yvo <yvozwijnenburg@icloud.com>
2024-09-10 13:49:50 +0200

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<div class="bg-neutral-800 text-neutral-200 font-po">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-neutral-800">
+        <ul class="flex flex-row justify-end h-16 text-xl items-center nav nav-tabs nav-stacked :bg neutral-800">
+            <li class="mx-6"><a class="underline" href="/">Home</a></li>
+            <li class="mx-6"><a href="reviews">Reviews</a></li>
+            <li class="mx-6"><a href="blog">Blog</a></li>
+            <li class="mx-6"><a href="about">About</a></li>
+            <li class="mx-6"><a href="contact">Contact</a></li>
+            <li>|</li>
+            <li class="mx-6"><a href="https://www.linkedin.com/in/yvo-zwijnenburg/"><img class="max-h-12" src="/img/linkedin.png" alt="linkedin logo"></a></li>
+            <li><a href=""><img src="" alt=""></a></li>
+        </ul>
+    </nav>
+</div>
+<body class="bg-neutral-800 text-neutral-200">
+<div>
+    <div class="container flex flex-col justify-end">
+        <h1>Yvo zwijnenburg</h1>
+    </div>
+</div>
+</body>
