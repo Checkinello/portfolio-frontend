d8c946a20647109b491698b0ef1d7ac7260b0442
yvo <yvozwijnenburg@icloud.com>
2024-09-16 13:16:03 +0200

    add front-end to butbuckit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,14 +8,22 @@
             <li class="mx-6"><a href="contact">Contact</a></li>
             <li>|</li>
             <li class="mx-6"><a href="https://www.linkedin.com/in/yvo-zwijnenburg/"><img class="max-h-12" src="/img/linkedin.png" alt="linkedin logo"></a></li>
-            <li><a href=""><img src="" alt=""></a></li>
         </ul>
     </nav>
 </div>
 <body class="bg-neutral-800 text-neutral-200">
-<div>
-    <div class="container flex flex-col justify-end">
-        <h1>Yvo zwijnenburg</h1>
+<div class="flex flex-row">
+    <div class="container  max-w-full max-h-96 my-40 flex flex-col items-center flex-wrap">
+        <div class="">
+        <h1 class="font-poppins text-5xl font-bold">Yvo Zwijnenburg</h1>
+        </div>
+        <div>
+            <p class="max-w-96">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nisl erat, scelerisque nec felis sit amet, ornare vestibulum massa. Praesent non lacinia sem. Nulla luctus euismod faucibus. Ut convallis libero sed interdum venenatis. Quisque accumsan odio a massa sagittis pretium. Fusce ultricies, ipsum vel congue posuere, libero lacus vulputate urna, eget varius enim orci placerat urna. Pellentesque leo mauris, malesuada non mi ac, convallis tincidunt tellus. Vivamus vulputate nisl aliquam neque dapibus, at tristique nisl efficitur. Duis ornare dictum lorem, et tristique nulla maximus eu. Fusce dignissim ut justo vel venenatis.</p>
+        </div>
+        <div class="container max-w-50">
+        <img class="max-w-96" src="/img/roy.png" alt="roy">
+        </div>
     </div>
+
 </div>
 </body>
