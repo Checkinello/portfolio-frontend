9869724d923a803a545b8ad85f2bcbec67df9756
yvo <yvozwijnenburg@icloud.com>
2024-09-23 11:57:41 +0200

    Made home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,22 +8,30 @@
             <li class="mx-6"><a href="contact">Contact</a></li>
             <li>|</li>
             <li class="mx-6"><a href="https://www.linkedin.com/in/yvo-zwijnenburg/"><img class="max-h-12" src="/img/linkedin.png" alt="linkedin logo"></a></li>
+            <li><button class="bg-neutral-200 text-neutral-800 p-2 font-bold rounded-2xl ml-6"><a href="">Sign up</a></button></li>
+            <li><button class="bg-neutral-800 text-neutral-200 p-2 font-bold rounded-2xl border-neutral-200 border-2 mx-4"><a href="">Sign in</a></button></li>
         </ul>
     </nav>
 </div>
 <body class="bg-neutral-800 text-neutral-200">
-<div class="flex flex-row">
-    <div class="container  max-w-full max-h-96 my-40 flex flex-col items-center flex-wrap">
-        <div class="">
-        <h1 class="font-poppins text-5xl font-bold">Yvo Zwijnenburg</h1>
+<div class="flex items-center justify-center min-h-screen p-4">
+    <div class="flex flex-col md:flex-row items-center max-w-6xl p-6 rounded-lg">
+        <!-- Text Container -->
+        <div class="flex-1 text-center md:text-left mb-6 md:mb-0">
+            <h1 class="font-poppins text-7xl font-bold mb-4">Yvo Zwijnenburg</h1>
+            <h2 class="font-poppins text-5xl text-neutral-400 font-bold mb-4">Software Developer</h2>
+            <p class="text-xl max-w-3xl mb-6">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nisl erat, scelerisque nec felis sit amet, ornare vestibulum massa. Praesent non lacinia sem. Nulla luctus euismod faucibus. Ut convallis libero sed interdum venenatis. Quisque accumsan odio a massa sagittis pretium. Fusce ultricies, ipsum vel congue posuere, libero lacus vulputate urna, eget varius enim orci placerat urna. Pellentesque leo mauris, malesuada non mi ac, convallis tincidunt tellus. Vivamus vulputate nisl aliquam neque dapibus, at tristique nisl efficitur. Duis ornare dictum lorem, et tristique nulla maximus eu. Fusce dignissim ut justo vel venenatis.
+            </p>
+            <div class="flex flex-col md:flex-row gap-4">
+                <button class="bg-neutral-200 text-neutral-800 p-2 font-bold rounded-2xl"><a href="about">View my projects</a></button>
+                <button class="bg-neutral-800 text-neutral-200 p-2 font-bold rounded-2xl border-neutral-200 border-2"><a href="contact">Contact Me</a></button>
+            </div>
         </div>
-        <div>
-            <p class="max-w-96">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nisl erat, scelerisque nec felis sit amet, ornare vestibulum massa. Praesent non lacinia sem. Nulla luctus euismod faucibus. Ut convallis libero sed interdum venenatis. Quisque accumsan odio a massa sagittis pretium. Fusce ultricies, ipsum vel congue posuere, libero lacus vulputate urna, eget varius enim orci placerat urna. Pellentesque leo mauris, malesuada non mi ac, convallis tincidunt tellus. Vivamus vulputate nisl aliquam neque dapibus, at tristique nisl efficitur. Duis ornare dictum lorem, et tristique nulla maximus eu. Fusce dignissim ut justo vel venenatis.</p>
-        </div>
-        <div class="container max-w-50">
-        <img class="max-w-96" src="/img/roy.png" alt="roy">
+        <!-- Image Container -->
+        <div class="flex-1 flex justify-center md:justify-end">
+            <img class="rounded-full border-4 border-neutral-200" src="/img/roy.png" alt="roy">
         </div>
     </div>
-
 </div>
 </body>
